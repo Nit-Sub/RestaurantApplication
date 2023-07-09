aca77211cae6e0d966425e3cee84b059ad65f182
Nit-Sub <52620899+Nit-Sub@users.noreply.github.com>
2023-07-09 12:30:06 +0545

    .

---
+++ src/container/Gallery/Gallery.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { BsInstagram , BsArrowLeftShort ,BsArrowRightShort } from 'react-icons/bs';
+import { SubHeading } from '../../components';
+import { images } from '../../constants';
+import './Gallery.css';
+
+const Gallery = () =>{
+  return(
+    <>
+    <div className='app__gallery'>
+    </div>
+    </>
+  )
+}
+
+export default Gallery;
