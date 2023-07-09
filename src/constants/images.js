003abe49fd7a2c6a52385f67bf29fe7cfa458ff6
Nit-Sub <52620899+Nit-Sub@users.noreply.github.com>
2023-07-09 12:38:04 +0545

    .

--- src/constants/images.js
+++ src/constants/images.js
@@ -20,7 +20,6 @@ import award05 from '../assets/award05.png';
 import sign from '../assets/sign.png';
 import quote from '../assets/quote.png';
 import gericht from '../assets/gericht.png';
-
 export default {
   bg,
   chef,
